a245284e1f5e305f582f3d16f96163e6352f9341
anders617 <anders617@users.noreply.github.com>
2016-10-27 14:30:34 -0400

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -7,18 +7,16 @@ var module = angular.module("MichiganTendies.controllers", []);
 
 
 module.controller("MichiganTendiesController", [
-    "$scope", "$interval", "$filter", "Item", "foodAliases", "MDiningAPI", "MDiningData", "GoogleMaps", "MDiningDataFilter",
-    function ($scope, $interval, $filter, Item, foodAliases, MDiningAPI, MDiningData, GoogleMaps, MDiningDataFilter) {
-        var menuList = this;
-        $scope.filteredItems = [];
-        $scope.allowInput = false;
+    "$scope", "$interval", "MDiningAPI", "MDiningData", "GoogleMaps", "MDiningDataFilter", "defaultAttributes", "defaultDateRange",
+    function ($scope, $interval, MDiningAPI, MDiningData, GoogleMaps, MDiningDataFilter, defaultAttributes, defaultDateRange) {
+        var controller = this;
 
         /**
          *
          * @param {array} mealNames
          * @returns {string} string list of meals in array
          */
-        menuList.printableMealNames = function (mealNames) {
+        controller.printableMealNames = function (mealNames) {
             return mealNames.reduce(function (previousValue, currentValue, currentIndex, array) {
                 if (typeof currentValue === "undefined") {
                     return "";
@@ -37,24 +35,42 @@ module.controller("MichiganTendiesController", [
          * @param {DiningHallMatch} diningHallMatch
          * @returns {string} url for dining hall
          */
-        menuList.getGoogleMapsURL = function (diningHallMatch) {
+        controller.getGoogleMapsURL = function (diningHallMatch) {
             return GoogleMaps.getURL(diningHallMatch);
         };
 
-        menuList.filterItems = function (searchTerm) {
+        controller.filterItems = function (searchTerm) {
             $scope.totalDisplayed = 20;
             $scope.filteredItems = MDiningDataFilter.filterItemsWithKeyword(searchTerm);
         };
 
-        menuList.init = function () {
+        controller.updateFilters = function (attributes, startDate, endDate, diningHalls) {
+            console.log("UPDATING FILTER");
+            console.log(attributes);
+            console.log(startDate);
+            console.log(endDate);
+            startDate.setHours(0);
+            startDate.setMinutes(0);
+            startDate.setMilliseconds(0);
+            endDate.setHours(23);
+            endDate.setMinutes(59);
+            endDate.setMilliseconds(59);
+            $scope.totalDisplayed = 20;
+            $scope.filteredItems = MDiningDataFilter.filterItems($scope.searchTerm, attributes, startDate, endDate, diningHalls);
+            console.log($scope.filteredItems);
+        };
+
+        controller.init = function () {
+
             $scope.filteredItems = [];
             $scope.allowInput = false;
             $scope.isLoadingData = true;
             $scope.loadingPercentage = 0;
-            MDiningAPI.requestDiningData(function(progress) {
+            console.log(MDiningAPI);
+            MDiningAPI.requestDiningData(function (progress) {
                 $scope.loadingPercentage = Math.floor(100 * progress);
                 $scope.$apply();
-            },function () {
+            }, function () {
                 $scope.isLoadingData = false;
                 console.log(MDiningData);
                 var initialSearchTerm = "Tendies";
@@ -62,11 +78,15 @@ module.controller("MichiganTendiesController", [
                 var count = 0;
                 $interval(function () {
                     $scope.searchTerm += initialSearchTerm[count];
-                    menuList.filterItems($scope.searchTerm);
+                    controller.filterItems($scope.searchTerm);
                     count += 1;
                 }, 100, initialSearchTerm.length);
                 $scope.allowInput = true;
+
+                $scope.attributes = defaultAttributes;
+                $scope.diningHalls = MDiningData.diningHalls;
+                $scope.updateFilters = controller.updateFilters;
             });
         };
-        menuList.init();
+        controller.init();
     }]);
