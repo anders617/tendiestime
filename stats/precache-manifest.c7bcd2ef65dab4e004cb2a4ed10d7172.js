8ce7b87e4419ec0ef891088f8bc245c529f32848
anders617 <anders617@gmail.com>
2019-11-03 12:17:58 -0500

    Updated stats build

---
+++ stats/precache-manifest.c7bcd2ef65dab4e004cb2a4ed10d7172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861492b1d8fa75b5ce814afbe5a1d283",
+    "url": "/stats/index.html"
+  },
+  {
+    "revision": "6be64fe6dd7de1bc7467",
+    "url": "/stats/static/css/main.776c7266.chunk.css"
+  },
+  {
+    "revision": "54d38c8c1f420ea1f2f4",
+    "url": "/stats/static/js/2.464680d5.chunk.js"
+  },
+  {
+    "revision": "6be64fe6dd7de1bc7467",
+    "url": "/stats/static/js/main.1bdaf634.chunk.js"
+  },
+  {
+    "revision": "c940ee7a494193c1758f",
+    "url": "/stats/static/js/runtime-main.37f71620.js"
+  }
+]);
