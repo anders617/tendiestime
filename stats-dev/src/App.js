c71b69e2b74d30e7427b4778859f7c8d036704d7
anders617 <anders617@gmail.com>
2020-06-12 17:50:33 -0400

    Update menu link

--- stats-dev/src/App.js
+++ stats-dev/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <h2 style={{color: '#3F3F3F', fontSize: '16px', fontWeight: '200', fontFamily: 'Arial', fontStyle: 'italic', margin: '20px', marginTop: '5px'}}>
           Updated Daily <br />
           <p>Charts and statistics on food being served at University of Michigan dining halls.</p>
-          <p>If you are looking for michigan dining hall menus check out the <a href="/menus">menus page</a></p>
+          <p>If you are looking for michigan dining hall menus check out the <a href="/menu">menus page</a></p>
           <p>If you are looking for a searchable list of food check out <a href="/">tendiestime</a></p>
         </h2>
       </header>
